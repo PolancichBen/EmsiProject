6d2c1e91b1ce0eafae6f5c50fe84873f6386f9ce
Ben Polancich <BenPolancich@Bens-MacBook-Air.local>
2020-09-06 10:50:34 -0500

    setting up Css styling for each school

--- frontEnd/main/subComponents/schoolSubComponents/upperCard.jsx
+++ frontEnd/main/subComponents/schoolSubComponents/upperCard.jsx
@@ -1,27 +1,35 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import styles from './upperCard.css';
 
-const UpperCard = ({schoolName, schoolCity, schoolState, studentSize, admissionRate}) => {
+const UpperCard = ({ schoolName, schoolCity, schoolState, studentSize, admissionRate }) => {
+
   return (
-    <div>
-      <div>
-        Picture
+    <div className={styles.upperCardContainer}>
+      <div className={styles.image}>
+        <img src="./university.svg"></img>
       </div>
-      <div>
-        <div>{schoolName}</div>
-        <div>{schoolCity + ', ' + schoolState}</div>
-        <div>
-        <div>
-          <span>School Size:</span>
-          <span>{studentSize}</span>
+      <div className={styles.namingContainer}>
+        <div className={styles.schoolName}>{schoolName}</div>
+        <div className={styles.cityStateContainer}>
+          <div className={styles.cityState}>{schoolCity + ', ' + schoolState}</div>
         </div>
-        <div>
-          <span>Admission Rate:</span>
-          <span>{admissionRate * 10}</span>
-        </div>
+        <div className={styles.studentContainer}>
+          <div className={styles.schoolContainer}>
+            <span className={styles.studentLabel}>School Size:</span>
+            <span className={styles.studentCount}>{studentSize}</span>
+          </div>
+          <div className={styles.admissionContainer}>
+            <span className={styles.admissionLabel}>Admission Rate:</span>
+            <span className={styles.admissionCount}>{admissionRate * 10}</span>
+            <div id="admissionProgressContainer">
+              <div id="admissionProgress"></div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   )
 }
+
 
 export default UpperCard;
